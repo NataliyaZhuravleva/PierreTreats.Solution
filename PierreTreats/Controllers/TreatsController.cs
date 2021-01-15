a85db2dbd343c6e6afc2ab6a463dfcd225e45e40
Nataliya Zhuravleva <natalindria@gmail.com>
2021-01-15 10:21:14 -0800

    Add Treat Index and Create in controller and views

---
+++ PierreTreats/Controllers/TreatsController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PierreTreats.Models;
+using System.Linq;
+
+namespace PierreTreats.Controllers
+{
+  
+}
